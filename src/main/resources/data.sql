e389cedb4c877bb687ad7b844920653ad6f976d1
Ian Gurgel <iangurgel25@hotmail.com>
2026-01-18 18:33:01 -0300

    add spring.sql.init.mode=always

---
+++ src/main/resources/data.sql
@@ -0,0 +1,62 @@
+-- Aluno 1 - Alice
+INSERT INTO tb_grades (id, student_id, subject, value) VALUES
+                                                           (1, 1, 'Matemática', 8.5),
+                                                           (2, 1, 'Matemática', 7.0),
+                                                           (3, 1, 'História', 9.0),
+                                                           (4, 1, 'Geografia', 8.0);
+
+-- Aluno 2 - Bob
+INSERT INTO tb_grades (id, student_id, subject, value) VALUES
+                                                           (5, 2, 'Matemática', 6.0),
+                                                           (6, 2, 'Matemática', 5.5),
+                                                           (7, 2, 'História', 6.5),
+                                                           (8, 2, 'Geografia', 7.0);
+
+-- Aluno 3 - Carla
+INSERT INTO tb_grades (id, student_id, subject, value) VALUES
+                                                           (9, 3, 'Matemática', 9.0),
+                                                           (10, 3, 'História', 8.5),
+                                                           (11, 3, 'Geografia', 9.5);
+
+-- Aluno 4 - Daniel
+INSERT INTO tb_grades (id, student_id, subject, value) VALUES
+                                                           (12, 4, 'Matemática', 7.0),
+                                                           (13, 4, 'História', 6.0),
+                                                           (14, 4, 'Geografia', 6.5);
+
+-- Aluno 5 - Eduarda
+INSERT INTO tb_grades (id, student_id, subject, value) VALUES
+                                                           (15, 5, 'Matemática', 8.0),
+                                                           (16, 5, 'Matemática', 6.0),
+                                                           (17, 5, 'História', 7.5),
+                                                           (18, 5, 'Geografia', 8.0);
+
+-- Aluno 6 - Felipe
+INSERT INTO tb_grades (id, student_id, subject, value) VALUES
+                                                           (19, 6, 'Matemática', 9.0),
+                                                           (20, 6, 'História', 8.0),
+                                                           (21, 6, 'Geografia', 8.5);
+
+-- Aluno 7 - Gabriela
+INSERT INTO tb_grades (id, student_id, subject, value) VALUES
+                                                           (22, 7, 'Matemática', 5.0),
+                                                           (23, 7, 'Matemática', 6.0),
+                                                           (24, 7, 'História', 6.5);
+
+-- Aluno 8 - Henrique
+INSERT INTO tb_grades (id, student_id, subject, value) VALUES
+                                                           (25, 8, 'Matemática', 7.5),
+                                                           (26, 8, 'História', 7.0),
+                                                           (27, 8, 'Geografia', 6.5);
+
+-- Aluno 9 - Isabela
+INSERT INTO tb_grades (id, student_id, subject, value) VALUES
+                                                           (28, 9, 'Matemática', 9.5),
+                                                           (29, 9, 'História', 9.0),
+                                                           (30, 9, 'Geografia', 9.0);
+
+-- Aluno 10 - João
+INSERT INTO tb_grades (id, student_id, subject, value) VALUES
+                                                           (31, 10, 'Matemática', 6.0),
+                                                           (32, 10, 'História', 5.5),
+                                                           (33, 10, 'Geografia', 6.0);
